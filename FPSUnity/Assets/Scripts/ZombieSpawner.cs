01c974a2eb847a54b0665f3436c2d5231c288c36
WUStudent <wustudent@webster.edu>
2025-09-19 16:44:56 -0500

    Zombie Models

--- FPSUnity/Assets/Scripts/ZombieSpawner.cs
+++ FPSUnity/Assets/Scripts/ZombieSpawner.cs
@@ -7,7 +7,7 @@ public class ZombieSpawner : MonoBehaviour
 {
     public static ZombieSpawner Instance;
 
-    public GameObject zombiePrefab;
+    public List<GameObject> zombiePrefabs;
     public List<Transform> spawnPoints;
 
     int wave;
@@ -15,10 +15,18 @@ public class ZombieSpawner : MonoBehaviour
 
     public TextMeshProUGUI waveText;
 
+    Transform player;
+    public float minDistance = 5f;
+
     void Awake()
     {
         Instance = this;
         wave = 1;
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<FPSController>().transform;
     }
 
     // Update is called once per frame
@@ -35,10 +43,21 @@ public class ZombieSpawner : MonoBehaviour
         for (int i = 0; i < wave; i++)
         {
             int rand = Random.Range(0, spawnPoints.Count);
-            Instantiate(zombiePrefab, spawnPoints[rand].position, transform.rotation, transform);
-        }
+            Vector3 spawnPos = spawnPoints[rand].position;
+            int attempts = 0;
+            int maxAttempts = 10;
 
-        //Update HUD with Wave
+            while (Vector3.Distance(spawnPos, player.position) < minDistance && attempts < maxAttempts)
+            {
+                rand = Random.Range(0, spawnPoints.Count);
+                spawnPos = spawnPoints[rand].position;
+                attempts++;
+            }
+
+            int randZombie = Random.Range(0, zombiePrefabs.Count);
+
+            Instantiate(zombiePrefabs[randZombie], spawnPos, transform.rotation, transform);
+        }
     }
 
     public void CountZombies()
